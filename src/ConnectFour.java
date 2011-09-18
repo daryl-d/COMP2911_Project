641a6b2046215465d5975554ca07959c4f354d12
Jaiden Ashmore <jaidenkyleashmore@gmail.com>
2011-09-18 20:31:36 +1000

    Initiliase git repository

---
+++ src/ConnectFour.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jaiden Ashmore
+ *
+ */
+public class ConnectFour {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
